33100f391641d118486f53dcfcbd678d7f6e2f06
wildan phidrai-ngam <wildan8376@gmail.com>
2024-04-08 23:19:07 +0700

    test app.sqlite pushed

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BooksModule } from './books/books.module';
+import { Book } from './books/entities/book.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TypeOrmModule.forRoot({
+    type: 'sqlite',
+    database: '../app.sqlite',
+    entities: [Book],
+    synchronize: process.env.NODE_ENV != 'production',
+  }), BooksModule],
 })
-export class AppModule {}
+export class AppModule { }
