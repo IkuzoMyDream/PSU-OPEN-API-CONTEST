e95c674843caf46c85d9916e464b40de92d0f160
Farik <6610110223@psu.ac.th>
2024-04-19 21:58:03 +0700

    added entity dtudents

--- server/src/students/entities/student.entity.ts
+++ server/src/students/entities/student.entity.ts
@@ -4,4 +4,19 @@ import { Column, Entity } from "typeorm";
 export class Student {
     @Column({ unique: true })
     studentId: string
+    titleNameThai: string 
+    titleShortEng: string
+    studNameThai: string
+    studNameEng: string
+    studSnameThai: string
+    studSnameEng: string
+    studyLevelName: string
+    majorNameThai : string
+    majorNameEng : string
+    campusNameThai: string
+    campusNameEng: string
+    facNameThai: string
+    facNameEng: string
+    yearStatus: string
+    admitYear: string
 }
