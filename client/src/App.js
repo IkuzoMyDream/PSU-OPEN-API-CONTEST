ad59d7b5689b53b78d0ff170edb8de9ec7364aa8
wildan phidrai-ngam <wildan8376@gmail.com>
2024-04-23 17:05:34 +0700

    ฟยย

--- client/src/App.js
+++ client/src/App.js
@@ -6,17 +6,16 @@ import HomePage from "./pages/HomePage";
 function App() {
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="max-w-md w-full bg-white p-8 rounded-lg shadow-lg">
-        <h2 className="text-2xl font-semibold mb-4">Log in</h2>
-        <button
-          onClick={handleLogin}
-          className="w-full bg-blue-500 text-white font-semibold py-2 px-4 rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
-        >
-          กดเพื่อยืนยันตัวตน
-        </button>
-      </div>
-    </div>
+    <>
+    <Router>
+      <Routes>
+        <Route path="login" element={<LoginPage />} />
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/home" element={<HomePage />} />
+      </Routes>
+    </Router>
+  </>
+
   );
 }
 
