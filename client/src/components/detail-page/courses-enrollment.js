bea8eefc205cdeb0f4dc49a3ea7fe6cc438635fb
nawaphon sangthong <namaza545@gmail.com>
2024-04-24 03:15:38 +0700

    html text display courses-enrollment

--- client/src/components/detail-page/courses-enrollment.js
+++ client/src/components/detail-page/courses-enrollment.js
@@ -1,7 +1,53 @@
-export default function EnrollmentCourses() {
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useAuth } from "react-oidc-context";
+
+function EnrollmentCourses() {
+  const auth = useAuth();
+
+  const [studentDetail, setStudentDetail] = useState(null);
+  const fectStudentDetail = async () => {
+    const result = await axios.get(
+      `https://api-gateway.psu.ac.th/Test/regist/SubjectOfferCampus/01/1/2564?facID=15&deptID=202&limit=1000`,
+      {
+        headers: {
+          credential: "api_key=ZsB/vDqTm8vFOkyI1gYArrN/AGfXhqNT",
+          token: auth.user.access_token,
+        },
+      }
+    );
+    console.log("result = ", result);
+    setStudentDetail(result.data.data[0]);
+  };
+  useEffect(() => {
+    if (auth.isAuthenticated) {
+      fectStudentDetail();
+    }
+  }, [auth.user]);
+  //console.log(auth.user.access_token)
+  console.log(studentDetail);
   return (
-    <>
-      <>enrollment course page</>
-    </>
+    <div>
+      <h1>Student Detail</h1>
+      {studentDetail ? (
+        <div>
+          <div>
+            <p>เทอม: {studentDetail.eduTerm}</p>
+            <p>ปีการศึกษา: {studentDetail.eduYear}</p>
+            <p>รหัสวิชา: {studentDetail.subjectCode}</p>
+            <p>ชื่อวิชา: {studentDetail.subjectNameThai}({studentDetail.shortNameEng})</p>
+            <p>หน่วยกิต: {studentDetail.credit}</p>
+          </div>
+          <pre>{JSON.stringify(studentDetail, null, 2)}</pre> {/*json ทั้งหมดของapiนี้ (SubjectOfferCampus) */}
+        </div>
+      ) : (
+
+        <p>Loading...</p>
+      )}
+
+    </div>
   );
 }
+
+export default EnrollmentCourses;
+
