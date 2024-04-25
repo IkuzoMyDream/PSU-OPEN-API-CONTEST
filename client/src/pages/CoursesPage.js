4b067e4632ffb8bff0a1a354bf1f7ee0966559af
Farik <6610110223@psu.ac.th>
2024-04-25 21:17:40 +0700

    add 3   file page

---
+++ client/src/pages/CoursesPage.js
@@ -0,0 +1,18 @@
+import { NavBar } from "../components/navbar";
+
+
+function CoursesPage() {
+    
+  
+    
+   
+    return (
+
+      <div>
+        <NavBar/>
+        <p>คนอ่านเป็นเกย์</p>
+      </div>
+    );
+  }
+  
+  export default CoursesPage;
---
+++ client/src/pages/SubjectDetailPage.js
@@ -0,0 +1,18 @@
+import { NavBar } from "../components/navbar";
+
+
+function SubjectDetailPage() {
+    
+  
+    
+   
+    return (
+
+      <div>
+        <NavBar/>
+        <p>คนอ่านเป็นเกย์</p>
+      </div>
+    );
+  }
+  
+  export default SubjectDetailPage;
