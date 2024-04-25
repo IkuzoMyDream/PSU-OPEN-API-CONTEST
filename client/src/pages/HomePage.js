e92e41412165b9e58ba24fbd2406d6d7a12d58b1
wildan phidrai-ngam <wildan8376@gmail.com>
2024-04-26 00:30:02 +0700

    add chart

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -6,6 +6,29 @@ import { NavBar } from "../components/navbar";
 import StudentDetail from "../components/home-page/student-detail";
 import { axLOCAL, axPSU } from "../utils/config/ax";
 
+import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  ArcElement,
+} from "chart.js";
+import { Bar, Doughnut } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  ArcElement
+);
+
 function HomePage() {
   const auth = useAuth();
   const navigate = useNavigate();
@@ -21,7 +44,6 @@ function HomePage() {
   const fetchStudentProfileImage = async () => {
     try {
       const response = await axPSU.get(psuConfig.getStudentProfileImage);
-      console.log(response.data);
       setProfileImage(response.data);
     } catch (err) {
       console.log(err);
@@ -68,9 +90,67 @@ function HomePage() {
     <>
       <NavBar />
       <div className="container mx-auto sm-auto md-auto lg-auto px-20 py-10">
-        <div className="grid">
-          {profileImage && <img src={profileImage} />}
-          <StudentDetail studentDetail={studentDetail} />
+        <div className="grid grid-cols-4 gap-4">
+          <div className="col-span-1">
+            {profileImage && <img src={profileImage} />}
+            <StudentDetail studentDetail={studentDetail} />
+          </div>
+          <div className="col-span-3">
+            <div className="grid grid-cols-4 gap-3">
+              <div>
+                <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                  <div>เกรด</div>
+                </div>
+              </div>
+              <div>
+                <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                  <div>คะแนนอิง</div>
+                </div>
+              </div>
+              <div>
+                <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                  <div>ชั่วโมงกิจกรรม</div>
+                </div>
+              </div>
+              <div>
+                <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                  <div>หน่วยกิตสะสม</div>
+                </div>
+              </div>
+            </div>
+            <div className="grid grid-cols-2 gap-3">
+              <div>
+                <Bar
+                  data={{
+                    labels: ["เทอม1", `เทอม2`],
+                    datasets: [{ label: "my score", data: [5, 10] }],
+                  }}
+                />
+              </div>
+              <div>
+                <Doughnut
+                  data={{
+                    labels: ["my score"],
+                    datasets: [
+                      {
+                        label: "my score",
+                        data: [76, 24],
+                      },
+                    ],
+                  }}
+                  options={{
+                    layout: { padding: { bottom: 200 } },
+                    rotation: 270,
+                    circumference: 180,
+                    cutout: "60%",
+                    offset: 100,
+                    maintainAspectRatio: true,
+                    responsive: true,
+                  }}
+                />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
